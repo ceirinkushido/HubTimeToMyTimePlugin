c6865b63e624858842dce3817b8632f594bc85b0
ceirinkushido <32529984+ceirinkushido@users.noreply.github.com>
2018-09-19 02:32:15 +0100

    Add files via upload

--- popup.js
+++ popup.js
@@ -3,7 +3,81 @@ document.addEventListener('DOMContentLoaded', function() {
     checkPageButton.addEventListener('click', function() {
   
         chrome.tabs.executeScript( null, {
-           code: "var date = new Date(); var now_utc = date.getUTCHours(); var timeDiff = date.getHours() - now_utc; var x = document.getElementsByClassName('title may-blank'); for(i=0;i<x.length;i++){ if(x[i].innerHTML.includes('UTC')){ var str = x[i].innerHTML.substring(x[i].innerHTML.lastIndexOf('UTC')-1, x[i].innerHTML.lastIndexOf('UTC')-6); var str_og = str; var now = new Date(); var now_utc = Date.UTC( now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes(), now.getSeconds(), now.getMilliseconds()); str = str.replace(':',''); var now_hours = str.substring(0,2); var now_minutes = str.substring(2); var now_time = (parseInt(now_hours)*60)+parseInt(now_minutes); var timeDiff = 0; if(now_utc > now.getTime()){ timeDiff = (now_utc - now.getTime())/1000; timeDiff = timeDiff/60; timeDiff = (now_time + timeDiff)/60; now_time = ((timeDiff % 1)*60).toFixed(0); timeDiff = Math.floor(timeDiff); } else if(now_utc < now.getTime()){ timeDiff = (now.getTime() - now_utc)/1000; timeDiff = timeDiff/60; timeDiff = now_time - timeDiff; now_time = ((timeDiff % 1)*60).toFixed(0); timeDiff = Math.floor(timeDiff); } else{ timeDiff = now_hours; now_time = now_minutes; } if(now_time == 0){ now_time = '00';} x[i].innerHTML = x[i].innerHTML.replace(str_og + ' UTC', timeDiff + ':' + now_time + ' LOCAL TIME' ); } }"
+           code: "var date = new Date(); var now_utc = date.getUTCHours(); var timeDiff = date.getHours() - now_utc; var x = document.getElementsByClassName('title may-blank'); for(i=0;i<x.length;i++){ if(x[i].innerHTML.includes('UTC')){ var str = x[i].innerHTML.substring(x[i].innerHTML.lastIndexOf('UTC')-1, x[i].innerHTML.lastIndexOf('UTC')-6); var str_og = str; var now = new Date(); var now_utc = Date.UTC( now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes(), now.getSeconds(), now.getMilliseconds()); str = str.replace(':',''); var now_hours = str.substring(0,2); var now_minutes = str.substring(2); var now_time = (parseInt(now_hours)*60)+parseInt(now_minutes); var timeDiff = 0; if(now_utc > now.getTime()){ timeDiff = (now_utc - now.getTime())/1000; timeDiff = timeDiff/60; timeDiff = (now_time + timeDiff)/60; now_time = ((timeDiff % 1)*60).toFixed(0); timeDiff = Math.floor(timeDiff); } else if(now_utc < now.getTime()){ timeDiff = (now.getTime() - now_utc)/1000; timeDiff = timeDiff/60; timeDiff = (now_time - timeDiff)/60; now_time = ((timeDiff % 1)*60).toFixed(0); timeDiff = Math.floor(timeDiff); } else{ timeDiff = now_hours; now_time = now_minutes; } if(now_time.length ==  1){ now_time = '0'+now_time;} timeDiff=timeDiff+'';  if(timeDiff.length ==  1){ timeDiff = '0'+timeDiff;} x[i].innerHTML = x[i].innerHTML.replace(str_og + ' UTC', timeDiff + ':' + now_time + ' LOCAL TIME' );} }"
         }, function findDate(){} );
     }, false);
   }, false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+var date = new Date(); 
+var now_utc = date.getUTCHours(); 
+var timeDiff = date.getHours() - now_utc; 
+var x = document.getElementsByClassName('title may-blank'); 
+
+for(i=0;i<x.length;i++){ 
+    if(x[i].innerHTML.includes('UTC')){ 
+        var str = x[i].innerHTML.substring(x[i].innerHTML.lastIndexOf('UTC')-1, 
+        x[i].innerHTML.lastIndexOf('UTC')-6); 
+        
+        var str_og = str; 
+        var now = new Date(); 
+        var now_utc = Date.UTC( now.getFullYear(), 
+            now.getMonth(), 
+            now.getDate(), 
+            now.getHours(), 
+            now.getMinutes(), 
+            now.getSeconds(), 
+            now.getMilliseconds()); 
+
+        str = str.replace(':',''); 
+        
+        var now_hours = str.substring(0,2); 
+        var now_minutes = str.substring(2); 
+        var now_time = (parseInt(now_hours)*60)+parseInt(now_minutes); 
+        var timeDiff = 0; 
+        
+        if(now_utc > now.getTime()){ 
+            timeDiff = (now_utc - now.getTime())/1000; 
+            timeDiff = timeDiff/60; 
+            timeDiff = (now_time + timeDiff)/60; 
+            now_time = ((timeDiff % 1)*60).toFixed(0); 
+            timeDiff = Math.floor(timeDiff); 
+        } else if(now_utc < now.getTime()){ 
+            timeDiff = (now.getTime() - now_utc)/1000; 
+            timeDiff = timeDiff/60; 
+            timeDiff = (now_time - timeDiff)/60; 
+            now_time = ((timeDiff % 1)*60).toFixed(0); 
+            timeDiff = Math.floor(timeDiff); 
+        } else{ 
+            timeDiff = now_hours; now_time = now_minutes; 
+        } 
+        
+        if(now_time.length ==  0){ 
+            now_time = '0'+now_time;
+        } 
+        if(timeDiff.length ==  0){ 
+            timeDiff = '0'+timeDiff;
+        } 
+        
+        x[i].innerHTML = x[i].innerHTML.replace(str_og + ' UTC', timeDiff + ':' + now_time + ' LOCAL TIME' ); 
+    } 
+}
+*/
